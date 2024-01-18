da40a1c9f8610d1dad4dee0122c6985193aada93
Ganoben <ganonben@gmail.com>
2024-01-18 18:00:00 +0200

    working section 2

--- Ex_1/QNet.py
+++ Ex_1/QNet.py
@@ -1,39 +1,80 @@
 import tensorflow as tf
-from keras import layers
-import keras
-INPUT_SIZE = 4
-
-def build_model(layer_num, layer_sizes, output_size):
-    for i in range(layer_num):
-        if i == 0:
-            model = tf.keras.Sequential()
-            model.add(layers.Dense(layer_sizes[i], activation='relu', input_shape=(INPUT_SIZE,)))
-        else:
-            model.add(layers.Dense(layer_sizes[i], activation='relu'))
-    model.add(layers.Dense(output_size))
-    return model
+from tensorflow.keras import layers, models
 
 
-class QNet:
-    def __init__(self, layer_num, layer_sizes, output_size, optimizer, learning_rate=0.01):
-        self.model = build_model(layer_num, layer_sizes, output_size)
+class QNet(models.Model):
+    def __init__(self, layer_sizes, output_size, learning_rate=0.01, optimizer='adam'):
+        super(QNet, self).__init__()
+        self.model_layers = [layers.Dense(size, activation='relu') for size in layer_sizes]
+        self.output_layer = layers.Dense(output_size)
+
+        # Selecting the optimizer
         if optimizer == "adam":
             self.optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
         elif optimizer == "sgd":
             self.optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
         elif optimizer == "rmsprop":
             self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate)
-        self.model.compile(optimizer=self.optimizer, loss='mse')
-        self.loss_object = keras.losses.MeanSquaredError()
+        else:
+            raise ValueError(f"Optimizer {optimizer} not supported.")
+
+        # Compile the model
+        self.compile(optimizer=self.optimizer, loss='mean_squared_error')
+
+    def call(self, inputs):
+        x = inputs
+        for layer in self.model_layers:
+            x = layer(x)
+        return self.output_layer(x)
+
+    def custom_train_step(self, state, target):
+        with tf.GradientTape() as tape:
+            predictions = self(state, training=True)
+            loss = self.compiled_loss(target, predictions)
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        return loss
 
 
-    def forward(self, state):
-        return self.model(state)
 
-    def loss(self, reward, target):
-        return self.loss_object(reward, target)
-
-    def backwards(self, loss):
-        variables = self.model.trainable_variables
-        grads_and_vars = self.optimizer.compute_gradients(loss, variables)
-        self.optimizer.apply_gradients(grads_and_vars)
+# import tensorflow as tf
+# from keras import layers
+# import keras
+# INPUT_SIZE = 4
+#
+# def build_model(layer_num, layer_sizes, output_size):
+#     for i in range(layer_num):
+#         if i == 0:
+#             model = tf.keras.Sequential()
+#             model.add(layers.Dense(layer_sizes[i], activation='relu', input_shape=(INPUT_SIZE,)))
+#         else:
+#             model.add(layers.Dense(layer_sizes[i], activation='relu'))
+#     model.add(layers.Dense(output_size))
+#     return model
+#
+#
+# class QNet(tf.Module):
+#     def __init__(self, layer_num, layer_sizes, output_size, optimizer, learning_rate=0.01):
+#         super(QNet, self).__init__()
+#         self.model = build_model(layer_num, layer_sizes, output_size)
+#         if optimizer == "adam":
+#             self.optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+#         elif optimizer == "sgd":
+#             self.optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
+#         elif optimizer == "rmsprop":
+#             self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate)
+#         self.model.compile(optimizer=self.optimizer, loss='mse')
+#         self.loss_object = keras.losses.MeanSquaredError()
+#
+#
+#     def forward(self, state):
+#         return self.model(state)
+#
+#     def loss(self, reward, target):
+#         return self.loss_object(reward, target)
+#
+#     def backwards(self, y_i, rewards, tape):
+#         loss = self.loss_object(rewards, y_i)
+#         gradients = tape.gradient(loss, self.model.trainable_variables)
+#         self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+#
--- Ex_1/Section_2.py
+++ Ex_1/Section_2.py
@@ -1,22 +1,24 @@
 import gymnasium as gym
 import numpy as np
+
 from Params import Params
 import random
 from collections import deque
 from QNet import QNet
 N_REPLAY_SIZE = 100
-EPOCHS = 5000
+EPOCHS = 200
 LEARNING_RATE = 0.1
 LAYER_NUM = 3
 LAYER_SIZE = 24
 LAYER_SIZE_ARRAY = [LAYER_SIZE] * LAYER_NUM
 GREEDY_EPSILON = 0.5
 GREEDY_EPSILON_DECAY = 0.99
-MAX_ITERATION = 100
+MAX_ITERATION = 50
 DISCOUNT = 0.9
 BATCH_SIZE = 1
 UPDATE_TARGET_EVERY = 50
 DO_RENDER = False
+LOAD = False
 RENDER_METHOD = "human" if DO_RENDER else None
 
 
@@ -35,8 +37,12 @@ def train_agent():
     observation = env.observation_space
     action = env.action_space
     replay_q = initialize_experience_replay()
-    Qnet_target = QNet(layer_num=LAYER_NUM, layer_sizes=[24, 24, 24], output_size=action.n, optimizer="adam", learning_rate=0.01)
-    Qnet_values = QNet(layer_num=LAYER_NUM, layer_sizes=[24, 24, 24], output_size=action.n, optimizer="adam", learning_rate=0.01)
+    Qnet_target = QNet(layer_sizes=[24, 24, 24], output_size=action.n, optimizer="adam", learning_rate=0.01)
+    Qnet_values = QNet(layer_sizes=[24, 24, 24], output_size=action.n, optimizer="adam", learning_rate=0.01)
+    if LOAD:
+        Qnet_target.load_weights("Qnet_target.h5")
+        Qnet_values.load_weights("Qnet_values.h5")
+    dummy_outputs = Qnet_target.call(np.zeros((1, 4)))
     for episode_num in range(EPOCHS):
         state = env.reset()[0]
         state = np.reshape(state, (1, 4))
@@ -48,22 +54,27 @@ def train_agent():
             if rand < GREEDY_EPSILON:
                 action = np.random.choice([0, 1])
             else:
-                action = np.argmax(Qnet_values.forward(state))
+                action = np.argmax(Qnet_values.call(state))
             next_state, reward, done, _, _ = env.step(action)
+            next_state = np.reshape(next_state, (1, 4))
             replay_q.append((state, action, reward, next_state))
             replays = sample_batch(replay_q, BATCH_SIZE)
             replay_array = np.array([replay[0] for replay in replays]).reshape(len(replays), 4)
             reward_array = np.array([replay[2] for replay in replays]).reshape(len(replays), 1)
-            Qnet_outputs = Qnet_values.forward(replay_array)
+            Qnet_outputs = Qnet_values.call(replay_array)
             if done:
                 y_i = reward
             else:
                 y_i = reward + DISCOUNT * np.max(Qnet_outputs, axis=1)
-            loss = Qnet_values.loss(reward_array.squeeze(0), y_i)
-            Qnet_values.backwards(loss)
+            loss = Qnet_values.custom_train_step(replay_array, y_i)
             if iteration % UPDATE_TARGET_EVERY == UPDATE_TARGET_EVERY -1:
-                Qnet_target.model.set_weights(Qnet_values.model.get_weights())
+                #copy model parameters from Qnet_values to Qnet_target
+                Qnet_target.set_weights(Qnet_values.get_weights())
+
+
             state = next_state
+        print("Episode: {}, Loss: {}".format(episode_num, loss))
+    Qnet_target.save_weights("Qnet_target.h5")
 
 
 if __name__ == "__main__":
